0e09100f0f9f615030edeaffcedf612b01a9e3c7
Ivan Chabanov <firstchabanov@gmail.com>
2026-07-19 15:31:26 +0300

    refactor(camera): split YOLO adapter and surface

--- lib/app/di/app_di.dart
+++ lib/app/di/app_di.dart
@@ -1,10 +1,10 @@
 import 'package:get_it/get_it.dart';
 import 'package:pov_agent/app/bootstrap/app_runtime.dart';
-import 'package:pov_agent/app/widgets/observation_surface.dart';
 import 'package:pov_agent/core/constants/compilation_constants.dart';
 import 'package:pov_agent/features/camera/application/ports/observation_controller.dart';
 import 'package:pov_agent/features/camera/application/ports/recorded_observation_frame_source.dart';
 import 'package:pov_agent/features/camera/data/adapters/recorded_observation_adapter.dart';
+import 'package:pov_agent/features/camera/data/adapters/yolo_observation_adapter.dart';
 import 'package:pov_agent/features/camera/data/datasources/method_channel_recorded_video_frame_source.dart';
 import 'package:pov_agent/features/camera/data/datasources/permission_handler_camera_permission_gateway.dart';
 import 'package:pov_agent/features/camera/data/datasources/recorded_frame_inference.dart';
